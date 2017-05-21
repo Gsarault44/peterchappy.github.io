a969403e824d8e75d47dbdc4ee682282b888f1e6
Peter Chapman <tuf21741@temple.edu>
2017-05-21 17:47:05 -0400

    started working on refactoring nav and handling mobile nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,63 +1,22 @@
 import React from 'react';
 import NavigationItem from './NavigationItem';
-
-const styles = {
-    navbar : {
-        width: "100%",
-        height: "10%",
-        position: "absolute",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "align-self",
-        justifyContent: "space-around",
-        backgroundColor: "#1E1E1E",
-        color: "#fff",
-        padding: "20px 0"
-    },
-
-    navSectionLeft : {
-        height: "100%",
-        display: "flex",
-        alignSelf: "flex-start",
-        alignContent: "space-around",
-        flexDirection: "row",
-        color: "#fff",
-    },
-
-    navSectionRight: {
-        height: "100%",
-        display: "flex",
-        alignSelf: "flex-end",
-        alignContent: "space-around",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        color: "#fff",
-    },
-
-    header: {
-        //TODO figure out ::after
-    }
-}
+import "./Navigation.css"
 
 const renderNavItems = (navItems) => {
-    return navItems.map((navItem) => {
-        return(
-            <NavigationItem key={navItem} title={navItem} />
-        )
-    });
+    return navItems.map((navItem) => <NavigationItem key={navItem} title={navItem} />);
 }
 
 const Navigation = ({navItems}) => {
-
-    const { navbar, navSectionLeft, header, navSectionRight } = styles;
-
     return(
-        <div style={navbar}>
-            <div style={navSectionLeft}>
-                <h1 id={"header"} style={header}> C:\\ Chapman</h1>
+        <div className="navbar">
+            <div className="navSectionLeft">
+                <h1 id={"header"} className="header"> C:\\ Chapman</h1>
             </div>
-            <ul style={navSectionRight}>
+            <ul className="navSectionRight desktop">
                 {renderNavItems(navItems)}
+            </ul>
+            <ul className="navSectionRight mobile">
+                <div className="borderMenu"/>
             </ul>
         </div>
     );
